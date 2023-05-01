564cfd7f0aad35cf3549cd0daa612c1bfe0f001c
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-05-01 18:57:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 import java.util.Scanner;
+
+class Main {
+  public static void main (String[] args) {
+    Scanner in = new Scanner(System.in);
+    
+    System.out.print("How many random passwords do you want generated?");
+    int total = in.nextInt();
+    System.out.print("How many characters long do you want your password to be?");
+    int length = in.nextInt();
+    
+  }
+
+
