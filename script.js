9ec62958251f2670bf4d81680176d94806a67218
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-05-01 19:59:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,41 @@ class Main {
     System.out.print("How many characters long do you want your password to be?");
     int length = in.nextInt();
     
+    String[] randomPasswords = new String[total];
+    
+    for(int i - 0; i < length; i++) {
+      String randomPassword = "";
+      for(int j = 0; j < length; j++){
+    randomPassword += randomCharacter();
+  }
+randomsPasswords[i] = randomPassword;
+    }
+    
   }
 
-
+public static void printPasswords(String[] arr) {
+  for(int i = 0; i < arr.length; i++) {
+    System.out.println(arr[i]);
+  }
+}
 public static char randomCharacter() {
   
   int rand = (int) (Math.random()*62);
+
+  if (rand <= 9) {
+   int ascii = rand += 48;
+    return (char) (ascii);
+    
+  } else if (rand <= 35) {
+    
+    int ascii = rand + ;
+    return (char) (ascii);
+  } else {
+    
+    int ascii = rand + 61;
+    return (char) (ascii);
+  }
+  
+}
+
+}
